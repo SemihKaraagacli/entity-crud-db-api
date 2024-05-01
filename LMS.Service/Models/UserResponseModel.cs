79db461fba044c15a3aba43134af54de35063af3
SemihKaraagacli <karaagaclisemih@gmail.com>
2024-05-01 22:07:15 +0300

    Service request-response model add and configuration

---
+++ LMS.Service/Models/UserResponseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace LMS.Services.Models
+{
+    public class UserResponseModel
+    {
+
+        public string Email { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
+        public int? RoleId { get; set; }
+    }
+    
+}
